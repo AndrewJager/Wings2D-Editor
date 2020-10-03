1343ca4cfa7e20222ef3c43d4c7d6ad669b57d70
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2020-10-03 15:50:35 -0400

    Cleanup

--- src/main/java/com/wings2d/editor/ui/skeleton/treecontrols/SkeletonTreeControls.java
+++ src/main/java/com/wings2d/editor/ui/skeleton/treecontrols/SkeletonTreeControls.java
@@ -2,32 +2,12 @@ package com.wings2d.editor.ui.skeleton.treecontrols;
 
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.JTree;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.TreePath;
 
-import com.wings2d.editor.objects.skeleton.Skeleton;
-import com.wings2d.editor.objects.skeleton.SkeletonAnimation;
-import com.wings2d.editor.objects.skeleton.SkeletonBone;
-import com.wings2d.editor.objects.skeleton.SkeletonFrame;
 import com.wings2d.editor.objects.skeleton.SkeletonNode;
-import com.wings2d.editor.objects.skeleton.SkeletonPiece;
-import com.wings2d.editor.objects.skeleton.Sprite;
 import com.wings2d.editor.ui.skeleton.SkeletonDrawingArea;
 import com.wings2d.editor.ui.skeleton.SkeletonEdit;
 import com.wings2d.editor.ui.skeleton.SkeletonUIElement;
@@ -40,7 +20,6 @@ public class SkeletonTreeControls extends SkeletonUIElement{
 	private BoneControls boneControls;
 	private SpriteControls spriteControls;
 	private SkeletonTreeControlsUIElement currentCard;
-	private JTree tree;
 	private SkeletonDrawingArea drawingArea;
 	
 
@@ -61,7 +40,6 @@ public class SkeletonTreeControls extends SkeletonUIElement{
 		panel.add(boneControls.getPanel(), BoneControls.CARD_ID);
 		panel.add(spriteControls.getPanel(), SpriteControls.CARD_ID);
 		
-		this.tree = edit.getSkeletonTree().getTree();
 		this.drawingArea = edit.getDrawingArea();
 		
 		panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));

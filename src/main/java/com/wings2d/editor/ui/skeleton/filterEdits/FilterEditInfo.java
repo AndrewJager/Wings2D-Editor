666c9e300c2c076bd7e3a94d72478a9e7f915529
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2020-11-01 22:29:36 -0500

    Remove unused interface

--- src/main/java/com/wings2d/editor/ui/skeleton/filterEdits/FilterEditInfo.java
+++
@@ -1,7 +0,0 @@
-package com.wings2d.editor.ui.skeleton.filterEdits;
-
-import com.wings2d.framework.imageFilters.ImageFilter;
-
-public interface FilterEditInfo {
-	public abstract Class<? extends ImageFilter> getFilterClass();
-}

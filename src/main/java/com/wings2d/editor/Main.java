94238860de01db174bfb2503d74d25138305a5d6
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2020-11-15 20:07:38 -0500

    Remove unused import

--- src/main/java/com/wings2d/editor/Main.java
+++ src/main/java/com/wings2d/editor/Main.java
@@ -1,7 +1,6 @@
 package com.wings2d.editor;
 
 import com.wings2d.editor.ui.Editor;
-import com.wings2d.framework.imageFilters.FilterFactory;
 
 public class Main {
 	public static void main(String[] args) {

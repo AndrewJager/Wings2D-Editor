38d94cffd8a94ab7bd9395e7bcdb9454a89c1a2b
CatsAreEvil <35353143+CatsAreEvil@users.noreply.github.com>
2021-09-05 09:42:46 -0400

    Move ColorUtils to DBColor

--- src/main/java/com/wings2d/editor/objects/save/DBColor.java
+++ src/main/java/com/wings2d/editor/objects/save/DBColor.java
@@ -7,6 +7,20 @@ import java.sql.SQLException;
 
 public class DBColor extends DBValue<Color>{
 
+	public static class ColorUtils {
+		public static String toString(final Color col) {
+			return col.getRed() + ","
+					+ col.getGreen() + ","
+					+ col.getBlue() + ","
+					+ col.getAlpha();
+		}
+		public static Color fromString(final String str) {
+			String[] tokens = str.split(",");
+			return new Color(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),
+					Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
+		}
+	}
+
 	public DBColor(final Connection con, final String table, final String column) {
 		super(con, table, column);
 	}

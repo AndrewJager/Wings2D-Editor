cf5bd2198720101664acf9b86cacb58a6b53e65b
CatsAreEvil <35353143+CatsAreEvil@users.noreply.github.com>
2022-02-12 22:32:47 -0500

    Add ability to play animations

--- src/main/java/com/wings2d/editor/ui/skeleton/RenderAreaControls.java
+++ src/main/java/com/wings2d/editor/ui/skeleton/RenderAreaControls.java
@@ -4,11 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.border.EmptyBorder;
 
 import com.wings2d.editor.objects.skeleton.SkeletonFrame;
@@ -16,8 +18,16 @@ import com.wings2d.editor.ui.UIElement;
 
 public class RenderAreaControls extends UIElement<SkeletonEdit>{
 	private JButton render, previous, next;
+	private JToggleButton play;
 	private JLabel curFrame;
 	private JPanel idxPanel;
+	private Timer timer;
+	
+	private int curFrameTime, timeSinceLast;
+	private int TICK = 10;
+	
+	private String PLAY_CHAR = Character.toString('\u25B6');
+	private String STOP_CHAR = Character.toString('\u23F9');
 	
 	private SkeletonFrame renderFrame;
 
@@ -36,12 +46,28 @@ public class RenderAreaControls extends UIElement<SkeletonEdit>{
 		
 		previous = new JButton("<");
 		idxPanel.add(previous, BorderLayout.WEST);
-		curFrame = new JLabel("None");
-		curFrame.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+		curFrame = new JLabel("None", JLabel.CENTER);
 		idxPanel.add(curFrame, BorderLayout.CENTER);
 		next = new JButton(">");
 		idxPanel.add(next, BorderLayout.EAST);
-		panel.add(idxPanel, BorderLayout.EAST);
+		idxPanel.setBorder(new EmptyBorder(0, 80, 0, 80));
+		panel.add(idxPanel, BorderLayout.CENTER);
+		
+		play = new JToggleButton(PLAY_CHAR);
+		JPanel playPanel = new JPanel();
+		playPanel.add(play);
+		panel.add(playPanel, BorderLayout.EAST);
+		
+		
+		timer  = new Timer(TICK, 
+			new ActionListener() {
+				@Override
+			    public void actionPerformed(ActionEvent evt) {
+					updateTime();
+			    }
+			}
+		);
+		
 	}
 	
 	private void renderFrame() {
@@ -55,6 +81,31 @@ public class RenderAreaControls extends UIElement<SkeletonEdit>{
 			curFrame.setText(renderFrame.getName());
 			getEditPanel().getRenderArea().getRenderPanel().getDrawArea().setDrawItem(renderFrame);
 			getEditPanel().getRenderArea().getRenderPanel().getDrawArea().repaint();
+		}
+	}
+	
+	private void updateTime() {
+		timeSinceLast += TICK;
+		if (timeSinceLast >= curFrameTime) {
+			if (renderFrame != null) {
+				renderFrame = getEditPanel().getSkeleton().getNextFrame(renderFrame);
+				drawRender();
+				timeSinceLast = 0;
+				curFrameTime = getFrameTime();
+			}
+		}
+	}
+	private int getFrameTime() {
+		if (renderFrame != null) {
+			if (renderFrame.getTime() == 0) {
+				return getEditPanel().getEditor().getSettings().getDefaultTime();
+			}
+			else {
+				return renderFrame.getTime();
+			}
+		}
+		else {
+			return 0;
 		}
 	}
 
@@ -79,6 +130,21 @@ public class RenderAreaControls extends UIElement<SkeletonEdit>{
 				renderFrame = getEditPanel().getSkeleton().getNextFrame(renderFrame);
 				drawRender();
 			}
+		});
+		play.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (play.isSelected()) {
+					play.setText(STOP_CHAR);
+					timer.restart();
+					curFrameTime = getFrameTime();
+					timeSinceLast = 0;
+				}
+				else {
+					play.setText(PLAY_CHAR);
+					timer.stop();
+				}
+			}	
 		});
 	}
 }
